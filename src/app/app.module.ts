b8b7f978809949fc0807215037760c0038c09170
Bhushan Gandole <Bhushan.Gandole@citiustech.com>
2021-11-29 13:56:28 +0530

    patient details changes and side nav/dash changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Route, RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './shared/default/default.component';
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
+import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+import { NurseModule } from './nurse/nurse.module';
 
 const routes: Routes = [
   {
-    path: 'patient',
-    loadChildren: () =>
-      import('src/app/patient/patient.module').then((m) => m.PatientModule),
+    path: '',
+    component: DefaultComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+      },
+      {
+        path: 'nurse',
+        loadChildren: () =>
+          import('./nurse/nurse.module').then((m) => m.NurseModule),
+      },
+      {
+        path: 'patient',
+        loadChildren: () =>
+          import('src/app/patient/patient.module').then((m) => m.PatientModule),
+      },
+    ],
   },
 ];
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule,RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    RouterModule.forRoot(routes),
+    SharedModule,
+    NurseModule,
+    
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
