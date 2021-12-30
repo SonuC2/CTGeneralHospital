58c223245cde7d0917f32ae3ba750bb87314984a
ctbhushan <94034664+ctbhushan@users.noreply.github.com>
2021-12-30 15:32:31 +0530

    update in sidebar.ts

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -20,13 +20,8 @@ export class SidebarComponent  {
   showSubSubMenu: boolean = false;
   showPatientSubMenu: boolean = false;
   showEmployeeSubMenu : boolean = false;
-<<<<<<< HEAD
 
   isNurse : boolean = true;
-=======
-  showUserSubMenu : boolean = false;
-  isNurse : boolean = false;
->>>>>>> ccaba1498b63d769a0448b97a3af7b1c92110228
  
    constructor(private observer: BreakpointObserver) {}
  
