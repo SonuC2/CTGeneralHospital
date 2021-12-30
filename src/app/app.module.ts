927ad9202b76f7ed435f41e67bdd250a458386a0
ctbhushan <94034664+ctbhushan@users.noreply.github.com>
2021-12-30 15:23:45 +0530

    update changes in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,63 +8,37 @@ import { DashboardComponent } from './shared/dashboard/dashboard.component';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
 import { ScheduleModule } from '@syncfusion/ej2-angular-schedule';
-=======
-import { LoginregistrationHomepageComponent } from './user/loginregistration-homepage/loginregistration-homepage.component';
-
->>>>>>> ccaba1498b63d769a0448b97a3af7b1c92110228
 
 const routes: Routes = [
   {
     path: '',
     component: DefaultComponent,
-/*my changes defaultcomponent replace with login component*/
     children: [
-     
-    /* {
-        path: '',
-        component: LoginregistrationHomepageComponent,
-      },*/
       {
         path: '',
-        component: LoginregistrationHomepageComponent
-        
-      },
-     /* {
-        path: 'dashboard',
+
         component: DashboardComponent,
-        
-      },*/
+      },
       {
         path: 'nurse',
+
         loadChildren: () =>
           import('./nurse/nurse.module').then((m) => m.NurseModule),
       },
-
       {
         path: 'patient',
+
         loadChildren: () =>
           import('src/app/patient/patient.module').then((m) => m.PatientModule),
       },
-<<<<<<< HEAD
 
       {
         path: 'scheduling',
         loadChildren: () =>
-          import('src/app/scheduling/scheduling.module').then((m) => m.SchedulingModule),
-=======
-      /*my change*/
-      {
-        path: 'user',
-        loadChildren: () =>
-          import('src/app/user/user.module').then((m) => m.UserModule),
-      },
-      {
-        path: 'shared',
-        loadChildren: () =>
-          import('src/app/shared/shared.module').then((m) => m.SharedModule),
->>>>>>> ccaba1498b63d769a0448b97a3af7b1c92110228
+          import('src/app/scheduling/scheduling.module').then(
+            (m) => m.SchedulingModule
+          ),
       },
     ],
   },
